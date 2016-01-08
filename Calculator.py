7e7bbdad5ee7179ea79b84fc575c4405cb416974
adam-czarnik <aczarnik@2018.kehillah.org>
2016-01-08 10:34:28 -0800

    got the puns and quit and wrong selection working.

--- Calculator.py
+++ Calculator.py
@@ -3,30 +3,38 @@ import random
 
 x = 0
 
+def print_slow(str):
+    for letter in str:
+        print(letter)
+        time.sleep(.1)
+
 while x < 1:
-    print('---------')
+    print('-------------------------------------------')
     print('Selections:')
     print('1. Addition')
     print('2. Subtraction')
     print('3. Multiplication')
     print('4. Division')
-    print('5. Quit')
-    print('6. Lets have some fun with a math pun')
+    print('5. Lets have some fun with a math pun')
+    print('0. Quit')
     print('To make a selection type the corresponding number.')
 
     selection = input()
 
     if selection == '1':
+        #addition
         print('You selected addition. Congrats! You clearly didnt pass second grade.')
     elif selection == '2':
+        #subtraction
         print('You selected subtraction. The only reason this is here is because Mr. Kelley said I cant make the user add a negative number because I didnt put in a subtraction option. You should thank him.')
     elif selection == '3':
-        print('Lets multiply! ')
+        #multiplication
+        print('Lets multiply!')
     elif selection == '4':
-        print('Lets take a break from all this calculating and DIVIDE a pizza!')
-    elif selection == '5':
-        print('Goodbye.')
-        time.sleep(2)
+        #division
+        print('Lets take a break from all this calculating and DIVIDE a PI!')
+    elif selection == '0':
+        #quit
         print('5')
         time.sleep(1)
         print('4')
@@ -52,5 +60,26 @@ while x < 1:
         print('    |@| |@|  | |')
         print('  ___________|_|')
         x = 1
+    elif selection == '5':
+        #I only put in a few puns
+        puns = ['Some mathematicians are reluctant to cosine a loan.', 'I was kicked out of math class for one too many infractions.', 'A mathematician that couldnt stop adding up recently went incremental.', 'I used to hate math but then I realised decimals have a point.', 'I didnt understand the math, so the teacher summed it up for me.', 'What do organic mathematicians throw into their fireplaces? Natural Logs.', 'In high school I recall having a beautiful but difficult math teacher. She was easy on the eyes and hard on the pupils!', 'Mathematicians are sum worshippers.', 'I strongly dislike the subject of math however I am partial to fractions.', 'You know what happens after you miss math class? It starts adding up.']
+        print(random.choice(puns))
+    elif selection == 'thisisasecret':
+        print('[insert secret selection here]')
+        x = 1
+    elif selection == 'secretpun':
+        print('Hey! Put on a coat. Its like 20 degrees outside.')
+        time.sleep(4)
+        print('Aww, someone just made snow ANGLES')
+        time.sleep(4)
+        x = 1
     else:
-        print('You bein cray cray.')
+        print('Because you didnt make a correct selection, this program will self-destruct in 3 seconds.')
+        time.sleep(4)
+        print('3')
+        time.sleep(1)
+        print('2')
+        time.sleep(1)
+        print('1')
+        time.sleep(1)
+        x = 1
