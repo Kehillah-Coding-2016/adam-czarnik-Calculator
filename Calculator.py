555a4df09b345ed11d80557db1ba10dc4e5bde65
adam-czarnik <aczarnik@2018.kehillah.org>
2016-01-11 21:26:51 -0800

    squashed some bugs and fixed formatting

--- Calculator.py
+++ Calculator.py
@@ -22,29 +22,32 @@ def print_slow(str):
 while x < 1:
     #print(' ')
     print(' ')
-    print(bcolors.OKBLUE + bcolors.UNDERLINE + 'Selections:' + bcolors.ENDC)
-    print('1. Addition')
-    print('2. Subtraction')
-    print('3. Multiplication')
-    print('4. Division')
-    print('5. Quit')
-    print('6. Lets have some fun with a math pun!')
+    print(bcolors.OKBLUE + bcolors.BOLD + 'Selections:' + bcolors.ENDC)
+    print(bcolors.BOLD + '1.' + bcolors.ENDC +  ' Addition')
+    print(bcolors.BOLD + '2.' + bcolors.ENDC + ' Subtraction')
+    print(bcolors.BOLD + '3.' + bcolors.ENDC + ' Multiplication')
+    print(bcolors.BOLD + '4.' + bcolors.ENDC + ' Division')
+    print(bcolors.BOLD + '5.' + bcolors.ENDC + ' Quit')
+    print(bcolors.BOLD + '6.' + bcolors.ENDC + ' Lets have some fun with a math pun!')
     print(' ')
     #print(' ')
 
     selection = input()
 
-    if selection == '1' or 'addition' or 'add' or 'Addition' or 'Add':
+    if selection == '1' or selection == 'addition' or selection == 'add' or selection == 'Addition' or selection == 'Add':
         #addition
-        print('You selected addition. Congrats! You clearly didnt pass second grade.')
+        time.sleep(1)
+        print('You selected addition. Congrats! You clearly didnt pass second grade. ;)')
         time.sleep(2)
         add1 = input('Enter the first number to add: ')
         add2 = input('Enter the second number to add: ')
         addtotal = int(add1) + int(add2)
         print(str(addtotal) + ' is the total.')
         time.sleep(2)
-    elif selection == '2' or 'subtraction' or 'subtract' or 'Subtraction' or 'Subtract':
+    
+    elif selection == '2' or selection == 'subtraction' or selection == 'subtract' or selection == 'Subtraction' or selection == 'Subtract':
         #subtraction
+        time.sleep(1)
         print('You selected subtraction. The only reason this is here is because Mr. Kelley said I cant make the user add a negative number because I didnt put in a subtraction option. You should thank him.')
         time.sleep(2)
         sub1 = input('Enter the first number to subtract: ')
@@ -52,8 +55,10 @@ while x < 1:
         subtotal = int(sub1) - int(sub2)
         print(str(subtotal) + ' is the total.')
         time.sleep(2)
-    elif selection == '3' or 'multiplication' or 'multiply' or 'Multiplication' or 'Multiply':
+
+    elif selection == '3' or selection == 'multiplication' or selection == 'multiply' or selection == 'Multiplication' or selection == 'Multiply':
         #multiplication
+        time.sleep(1)
         print('Lets multiply!')
         time.sleep(2)
         mul1 = input('Enter the first number to multiply: ')
@@ -61,17 +66,21 @@ while x < 1:
         multotal = int(mul1) * int(mul2)
         print(str(multotal) + ' is the total.')
         time.sleep(2)
-    elif selection == '4' or 'division' or 'divide' or 'Division' or 'Divide':
+
+    elif selection == '4' or selection == 'division' or selection == 'divide' or selection == 'Division' or selection == 'Divide':
         #division
+        time.sleep(1)
         print('Lets take a break from all this calculating and DIVIDE a PI!')
         time.sleep(2)
         div1 = input('Enter the first number to divide: ')
         div2 = input('Enter the second number to divide: ')
-        divtotal = int(div1) + int(div2)
-        print(str(divtotal))
+        divtotal = int(div1) / int(div2)
+        print(str(divtotal) + ' is the total.')
         time.sleep(2)
-    elif selection == '5' or 'quit' or 'Quit' or 'godieinahole':
+
+    elif selection == '5' or selection == 'quit' or selection == 'Quit' or selection == 'godieinahole':
         #quit
+        time.sleep(1)
         print(bcolors.FAIL + '5' + bcolors.ENDC)
         time.sleep(1)
         print(bcolors.FAIL + '4' + bcolors.ENDC)
@@ -97,17 +106,21 @@ while x < 1:
         print('    |@| |@|  | |')
         print('  ___________|_|')
         x = 1
-    elif selection == '6' or 'math pun' or 'pun':
+
+    elif selection == '6' or selection == 'math pun' or selection == 'pun':
         #I only put in a few puns
         puns = ['Some mathematicians are reluctant to cosine a loan.', 'I was kicked out of math class for one too many infractions.', 'A mathematician that couldnt stop adding up recently went incremental.', 'I used to hate math but then I realised decimals have a point.', 'I didnt understand the math, so the teacher summed it up for me.', 'What do organic mathematicians throw into their fireplaces? Natural Logs.', 'In high school I recall having a beautiful but difficult math teacher. She was easy on the eyes and hard on the pupils!', 'Mathematicians are sum worshippers.', 'I strongly dislike the subject of math however I am partial to fractions.', 'You know what happens after you miss math class? It starts adding up.']
         print(random.choice(puns))
         time.sleep(2)
+
     elif selection == 'secretpun' or selection == 'secret' or selection == 'easteregg':
+        print('You found the secret! ' + bcolors.HEADER + 'CONGRATS!!' + bcolors.ENDC)
+        time.sleep(2)
         print('Hey! Put on a coat. Its like 20 degrees outside.')
         time.sleep(2)
-        print('Aww, someone just made snow ANGLES')
+        print('Aww, someone just made snow ANGLES!')
         time.sleep(2)
-        x = 1
+
     else:
         print(bcolors.FAIL + 'Because you didnt make a correct selection, this program will self-destruct in 3 seconds.' + bcolors.ENDC)
         time.sleep(2)
